fef1d0c91935d33a8c42a8df362fed5d5a31e081
prettyLove <574286814@qq.com>
2019-09-06 22:00:40 +0800

    mpvue

---
+++ npvueTest/src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+       我是首页
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return { }
+  },
+  methods: {},
+
+  created () {}
+}
+</script>
+
+<style scoped>
+
+</style>
